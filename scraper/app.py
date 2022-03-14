f61fbf21308ddc0a0e9fd8cd1e1138227e710b97
grace-pham <gracepham208@gmail.com>
2022-03-14 00:41:58 -0400

    Add parameter departure_date to runScraper

--- scraper/app.py
+++ scraper/app.py
@@ -2,24 +2,27 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-def runScraper(origin, dest):
+
+def runScraper(origin, dest, departure_date):
     # TODO: Scrape the data here
     return [
         {
-            "origin": origin, 
+            "origin": origin,
             "dest": dest,
+            "departure_date": departure_date,
             "train_name": "train123",
             "scheduled_departure": "time and date here",
             "scheduled_arrival": "time and date here",
         },
         {
-            "origin": origin, 
+            "origin": origin,
             "dest": dest,
             "train_name": "train145",
             "scheduled_departure": "time and date here",
             "scheduled_arrival": "time and date here",
         },
     ]
+
 
 # http://127.0.0.1:8080/?origin=Philly&destination=NewYork
 @app.route("/")
@@ -28,6 +31,7 @@ def index():
     dest = request.args.get('destination')
     res = runScraper(origin, dest)
     return jsonify(res)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
