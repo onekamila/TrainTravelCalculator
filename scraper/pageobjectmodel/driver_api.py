8579a5d834891417ba74732d5221a0dce418d480
grace-pham <gracepham208@gmail.com>
2022-03-15 01:30:54 -0400

    Update DriverAPI

--- scraper/pageobjectmodel/driver_api.py
+++ scraper/pageobjectmodel/driver_api.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,7 +8,6 @@ class DriverAPI:
 
     def __init__(self, driver):
         self.driver = driver
-        # self.driver.maximize_window()
 
     def get_link(self, url):
         self.driver.get(url)
@@ -18,26 +18,30 @@ class DriverAPI:
         element.click()
         return element
 
-    def send_data(self, element_method, data, method_used=By.CSS_SELECTOR, timeout=10):
-        element = self.find(element_method, method_used, timeout)
+    def send_data(self, element_method, data, method_used=By.CSS_SELECTOR):
+        element = self.get_element(element_method, method_used)
         element.send_keys(data)
         return element
+
+    def get_element(self, element_method, method_used=By.CSS_SELECTOR):
+        return self.find(element_method, method_used)
 
     def find(self, element_method, method_used=By.CSS_SELECTOR, timeout=10):
         try:
             element = WebDriverWait(self.driver, timeout).until(
                 EC.presence_of_element_located((method_used, element_method)))
+
             return element
         except:
             return None
 
     def get_text(self, element_method, method_used=By.CSS_SELECTOR, timeout=10):
-        content = self.find(element_method, method_used, timeout)
+        content = self.get_element(element_method, method_used, timeout)
         return content.text
 
     def is_present_on_page(self, element_method, method_used=By.CSS_SELECTOR, timeout=10):
 
-        if self.find(element_method, method_used, timeout) is not None:
+        if self.get_element(element_method, method_used) is not None:
             return True
         else:
             return False
@@ -48,15 +52,6 @@ class DriverAPI:
     def quit(self):
         self.driver.quit()
 
-    def find_list_element(self, element_method, method_used=By.CSS_SELECTOR, timeout=10):
-        try:
-            WebDriverWait(self.driver, timeout).until(
-                EC.element_to_be_clickable((method_used, element_method)))
-            elements = self.driver.find_elements(by=By.CSS_SELECTOR, value=element_method)
-            return elements
-        except:
-            return None
-
-    def check_url(self, url, time_out=10):
-        wait = WebDriverWait(self.driver, time_out)
-        # return wait.until(EC.url_to_be(url))
+    def click_enter(self, element_method, method_used=By.CSS_SELECTOR, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((method_used, element_method))).send_keys(Keys.ENTER)
