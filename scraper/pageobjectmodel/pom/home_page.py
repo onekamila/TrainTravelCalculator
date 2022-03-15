d02eed752b70ed20c831c82ef3ab287038f2ce14
grace-pham <gracepham208@gmail.com>
2022-03-14 21:09:26 -0400

    Edit home page po

--- scraper/pageobjectmodel/pom/home_page.py
+++ scraper/pageobjectmodel/pom/home_page.py
@@ -1,5 +1,5 @@
 from scraper.pageobjectmodel.pom.base.base_page import BasePage
-from scraper.pageobjectmodel.scrapping.config import Config
+from scraper.pageobjectmodel.execute.config import Config
 
 
 class HomePage(BasePage):
