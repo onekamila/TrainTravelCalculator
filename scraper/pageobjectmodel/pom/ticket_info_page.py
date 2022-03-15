897e4f4563bb50817b3b49854caee8f0c2a78343
grace-pham <gracepham208@gmail.com>
2022-03-15 01:52:13 -0400

    Edit Ticket Info Page

--- scraper/pageobjectmodel/pom/ticket_info_page.py
+++ scraper/pageobjectmodel/pom/ticket_info_page.py
@@ -6,16 +6,21 @@ from selenium.webdriver.common.by import By
 class TicketInformationPage(BasePage):
     expected_url = Config.ticketInfoUrl
 
-    # CSS Selector
     train_number_list = "train-info span:nth-child(1)"
     departure_time_list = "div.departure .departure-details span.font-light"
     arrival_time_list = ".arrival .departure-details span.font-light"
 
+    def __init__(self, driver, is_present=True):
+        super().__init__(driver=driver, is_present=is_present)
+
+    def is_present(self):
+        return self.expected_url == self.driver.get_current_url()
+
     def get_train_number(self):
-        self.driver.get_text(self.train_number_list)
+        self.driver.get_text_for_elements(self.train_number_list)
 
     def get_departure_time(self):
-        self.driver.get_text(self.departure_time_list)
+        self.driver.get_text_for_elements(self.departure_time_list)
 
     def get_arrival_time(self):
-        self.driver.get_text(self.arrival_time_list)
+        self.driver.get_text_for_elements(self.arrival_time_list)
