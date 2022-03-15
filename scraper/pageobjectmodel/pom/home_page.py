9aa770f59f5760df4a02fa6212b45097d2efc652
grace-pham <gracepham208@gmail.com>
2022-03-15 01:32:01 -0400

    Edit HomePage CSS

--- scraper/pageobjectmodel/pom/home_page.py
+++ scraper/pageobjectmodel/pom/home_page.py
@@ -8,9 +8,7 @@ class HomePage(BasePage):
     input_origin_field_CSS = "#page-content  .amtrak-ff-body .from-station input"
     input_destination_field_CSS = "#mat-input-1"
     input_departure_date_CSS = "#mat-input-2"
-    origin_dropdown_CSS = "#autocomplete__0 div:nth-child(1) > div.am-dropdown__option.am-simple-dropdown__option.ng-star-inserted"
-    destination_dropdown_CSS = "#autocomplete__1 > div > div > div:nth-child(1) > div:nth-child(2)"
-    search_train_button_CSS = "button.search-btn.ng-star-inserted"
+    search_train_button_CSS = "button.search-btn"
 
     def __init__(self, driver, is_present=True):
         super().__init__(driver=driver, is_present=is_present)
