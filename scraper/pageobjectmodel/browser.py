d08801bc4057294746e141494dda7bccdd5e8822
grace-pham <gracepham208@gmail.com>
2022-03-18 16:08:08 -0400

    Edit PATH to driver

--- scraper/pageobjectmodel/browser.py
+++ scraper/pageobjectmodel/browser.py
@@ -5,5 +5,5 @@ from selenium.webdriver.chrome.options import Options as COptions
 def get_browser(headless=True):
     options = COptions()
     options.headless = headless
-    driver = webdriver.Chrome(options=options, executable_path="../../drivers/chromedriver")
+    driver = webdriver.Chrome(options=options, executable_path="drivers/chromedriver")
     return driver
