7c6ed289522e3e37670f5a27fc225cd7e751ab30
Heidi Tran <HeidiTran1410@gmail.com>
2022-03-18 23:43:04 -0400

    update oracle conn

--- train-tracker/Controllers/TrainTrackController.cs
+++ train-tracker/Controllers/TrainTrackController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
+using Dapper;
 
 namespace train_tracker.Controllers
 {
@@ -21,17 +24,47 @@ namespace train_tracker.Controllers
         [HttpPost]
         public IEnumerable<TrainInformation> Post([FromBody]TrainQuery query)
         {
+            string departureDelay = null;
+            string arrivalDelay = null;
+			try
+			{
+                var conn = this.GetConnection();
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    var query = "GET_HISTORICAL_ARRIVAL_DELAY";
+                    departureDelay = SqlMapper.Query(conn, query, commandType: CommandType.StoredProcedure);
+
+                    query = "GET_HISTORICAL_DEPARTURE_DELAY";
+                    arrivalDelay = SqlMapper.Query(conn, query, commandType: CommandType.StoredProcedure);
+                }
+            }
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
             TrainInformation trainInfo = new()
             {
-                TrainName = "123",
-                RouteName = "North East Regional",
-                HistoricalDepartureDelay = "1 - 2 min",
-                HistoricalArrivalDelay = "0 min",
+                TrainName = query.TrainId,
+                HistoricalDepartureDelay = departureDelay,
+                HistoricalArrivalDelay = arrivalDelay,
                 ScheduledDeparture = query.ScheduledDeparture,
                 ScheduledArrival = query.ScheduledArrival
             };
 
             return new List<TrainInformation>() { trainInfo };
         }
+    }
+
+    public IDbConnection GetConnection()
+    {
+        var connectionString = configuration.GetSection("ConnectionStrings").GetSection("TrainDataConnection").Value;
+        var conn = new OracleConnection(connectionString);
+        return conn;
     }
 }
