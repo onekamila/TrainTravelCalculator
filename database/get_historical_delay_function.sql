7962c2d5f3f4357a053bcb4ecfa8fa6ab0f138b6
Heidi Tran <HeidiTran1410@gmail.com>
2022-03-18 22:39:33 -0400

    simplify functions

--- database/get_historical_delay_function.sql
+++ database/get_historical_delay_function.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE FUNCTION GET_HISTORICAL_DEPARTURE_DELAY (
-    origin_train_id IN NUMBER, 
+    train_id IN NUMBER, 
     origin_station_id IN NUMBER, 
-    dest_train_id IN NUMBER, 
     dest_station_id IN NUMBER)
     RETURN NUMBER IS
     v_origin_avg_delay NUMBER;
@@ -10,13 +9,13 @@ BEGIN
     SELECT AVG(a_depart - s_depart) 
     INTO v_origin_avg_delay 
     FROM Stop 
-    WHERE train_id = origin_train_id
+    WHERE train_id = train_id
     AND station_id = origin_station_id;
 
     SELECT AVG(a_depart - s_depart) 
     INTO v_dest_avg_delay 
     FROM Stop 
-    WHERE train_id = dest_train_id
+    WHERE train_id = train_id
     AND station_id = dest_station_id;
 
     RETURN (v_origin_avg_delay + v_dest_avg_delay) /2;
@@ -24,9 +23,8 @@ END GET_HISTORICAL_DEPARTURE_DELAY;
 \
 
 CREATE OR REPLACE FUNCTION GET_HISTORICAL_ARRIVAL_DELAY (
-    origin_train_id IN NUMBER, 
+    train_id IN NUMBER, 
     origin_station_id IN NUMBER, 
-    dest_train_id IN NUMBER, 
     dest_station_id IN NUMBER)
     RETURN NUMBER IS
     v_origin_avg_delay NUMBER;
@@ -35,13 +33,13 @@ BEGIN
     SELECT AVG(a_arrive - s_arrive) 
     INTO v_origin_avg_delay 
     FROM Stop 
-    WHERE train_id = origin_train_id
+    WHERE train_id = train_id
     AND station_id = origin_station_id;
 
     SELECT AVG(a_arrive - s_arrive) 
     INTO v_dest_avg_delay 
     FROM Stop 
-    WHERE train_id = dest_train_id
+    WHERE train_id = train_id
     AND station_id = dest_station_id;
 
     RETURN (v_origin_avg_delay + v_dest_avg_delay) /2;
