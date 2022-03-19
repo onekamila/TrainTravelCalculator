1aa7d6506913a3caa3b2366fea2dc7dfb6a47d48
grace-pham <gracepham208@gmail.com>
2022-03-18 23:21:45 -0400

    Update documentation

--- scraper/pageobjectmodel/execute/scraper_execution.py
+++ scraper/pageobjectmodel/execute/scraper_execution.py
@@ -12,21 +12,29 @@ class ScraperExecution:
         self.driver = DriverAPI(driver)
 
     def search_train(self, origin_info, destination_info, date_info):
+        # Navigate to Amtrak Home Page
         self.driver.get_link(Config.homepageUrl)
+        # Create object of Home Page
         home = HomePage(self.driver)
+        # Click on option to search for train status
         home.click_option_check_train_status()
+        # Enter train information to search
         home.fill_origin(origin_info)
         home.fill_destination(destination_info)
         home.choose_date(date_info)
+        # Click search button
         home.click_search_train_button()
         time.sleep(10)  # Time sleep for page relocating and page loading
         return self.scrape_train_data()
 
     def scrape_train_data(self):
+        # Create object of Ticket Information Page
         ticket = TicketInformationPage(self.driver)
+        # Get train information
         train_number_list = ticket.get_train_number()
         departure_time_list = ticket.get_departure_time()
         arrival_time_list = ticket.get_arrival_time()
+        # Reformat train information to serve backend input
         train_data = self.reformat_train_info(train_number_list, departure_time_list, arrival_time_list)
         return train_data
 
